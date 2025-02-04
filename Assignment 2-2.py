050b6d3ed3902af6432631f5cccae0e859b97c80
devon_love <devon-love@hotmail.com>
2025-02-04 11:44:09 -0500

    Chng2

---
+++ Assignment 2-2.py
@@ -0,0 +1,6 @@
+myCurrentAge = input("What is your age?")
+currentYear = input("What year is it?")
+futureYear = 2050
+yeardiff = int(futureYear) - int(currentYear)
+Agein2025 = int(myCurrentAge) + yeardiff
+print(Agein2025)
