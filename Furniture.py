48baa62a3f5b93d3b8dd266a1cbe844a6cb77de3
devon_love <devon-love@hotmail.com>
2025-02-04 12:51:11 -0500

    Chng1

---
+++ Furniture.py
@@ -0,0 +1,8 @@
+retailPrice = 325.00
+wholesalePrice = 200.00
+profit = retailPrice - wholesalePrice
+sale = retailPrice * 0.25
+salePrice = retailPrice - sale
+saleProfit = salePrice - wholesalePrice
+print("Item Name : TV Stand")
+print("Retail Price:" , retailPrice)
