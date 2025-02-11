96c96743572b58887824b33565ea0662a95ace4f
devon_love <devon-love@hotmail.com>
2025-02-11 15:02:09 -0500

    Assn 3-3 Chng 0

---
+++ Assignment 3-3.py
@@ -0,0 +1,23 @@
+#The purpose of this program is to determine as to whether the DTG provided by the User is valid or not.
+
+validDate=True
+minYear=0
+minMonth=1
+maxMonth=12
+minDay=1
+maxDay=31
+
+year=int(input("Enter the year (0001-9999): "))
+month=int(input("Enter the month (1-12): "))
+day=int(input("Enter the day (1-31): "))
+
+if int(year) <= minYear:
+    validDate=False
+elif int(month) < minMonth or int(month) > maxMonth:
+    validDate=False
+elif int(day) < minDay or int(day) > maxDay:
+    validDate=False
+if validDate == True:
+    print("The DTG provided is valid.")
+else:
+    print("The DTG provided is invalid.")
