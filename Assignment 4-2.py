874aebcdbee35bba2ff49723e1126532319f697b
devon_love <devon-love@hotmail.com>
2025-02-18 19:28:41 -0500

    Draft-1

---
+++ Assignment 4-2.py
@@ -0,0 +1,21 @@
+#
+#User Inputs for Employee Name, Shifts and Transactions
+Fname=input("Enter First Name of Employee: ")
+Lname=input("Enter Last Name of Employee: ")
+empName=Fname + " " + Lname
+numShifts=int(input("Enter the number of shifts: "))
+numTrans=int(input("Enter the number of transactions: "))
+transDollars=int(input("Enter the total dollar amount of transactions: "))
+
+#Calculate Employee's Productivity Score
+def transAvg=transDollars/numTrans
+def prodScore=transAng/numShifts
+
+#Calculate Employee's Bonus
+empBonus=0
+if 
+
+print("Employee's Name: " + empName)
+print(f'Number of shifts: {numShifts}')
+print(f'Number of transactions: {numTrans}')
+print(f'Total dollar value: ${transDollars}')
