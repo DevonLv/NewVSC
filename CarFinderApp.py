b44b4c384ad8ccd0e470db16331f03bc39dd8898
devon_love <devon-love@hotmail.com>
2025-04-08 13:18:46 -0400

    draft 6

--- CarFinderApp.py
+++ CarFinderApp.py
@@ -1,4 +1,4 @@
-## AutoCountry Car Finder v0.1 ##
+## AutoCountry Car Finder v0.2 ##
 # This is a simple car finder application that allows users to search for cars based on various criteria.
 
 # Declarations
@@ -20,11 +20,10 @@ def searchVehicle(vehicle_name):
     #Searches for a vehicle in the authorized list
     if vehicle_name in AllowedVehiclesList:
         return f"'{vehicle_name}' is an authorized vehicle."
-        print(display_menu()) 
     #If the vehicle is not found, return a message indicating that it is not authorized.
     else:
         return f"'{vehicle_name}' is NOT an authorized vehicle."
-        print(display_menu())
+
 
 #Display the menu options
 print(display_menu())
@@ -32,26 +31,28 @@ print(display_menu())
 #Execute the menu functions
 while True:
     try:
-        choice = int(input("Please select an option:"))
-            #print the list of authorized vehicles
+        choice = int(input("Please select an option: "))
+        #print the list of authorized vehicles
         if choice == 1:
-                print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
-                for item in AllowedVehiclesList:
-                    print(item)
-                print(display_menu())
-                choice = int(input("Please select an option:"))
-                
-            #print the selected vehicle
+            print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
+            for item in AllowedVehiclesList:
+                print(item)
+            print(display_menu())
+
+        #print the selected vehicle
         elif choice == 2:
-                searchRequest = input("Please ENTER the full vehicle name: ")
-                result = searchVehicle(searchRequest)
-                print(result)
-                print(display_menu())
-            
-            #exit the while loop, ending the program.
+            searchRequest = input("Please ENTER the full vehicle name: ")
+            result = searchVehicle(searchRequest)
+            print(result)
+            print(display_menu())
+
+        #exit the while loop, ending the program.
         elif choice == 3:
-                print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
-                break 
-            #Error catching for invalid input
+            print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+            break
+
+        #Error catching for invalid input
+        else:
+            print("Invalid choice. Please enter 1, 2 or 3.")
     except ValueError:
-        print("Invalid choice. Please enter 1, 2 or 3.")
+        print("Invalid input. Please enter a number for your choice.")
